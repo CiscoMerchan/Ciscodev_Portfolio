cf1524ba6539d79c0f43706363bd7f4dd55a53c6
Franciscoelias <franciscoeliasm@yahoo.com>
2022-11-01 23:51:01 +1000

    Clearing the code

--- porfolio.py
+++ porfolio.py
@@ -11,37 +11,14 @@ class Contactform(FlaskForm):
     message = TextAreaField('Message', validators=[DataRequired(), length(max=400)])
     submit = SubmitField('Send')
 
-class AboutMe(FlaskForm):
-    submit = SubmitField("My name is Franciscoelias ...")
-
-"""About me Text"""
-about_me = "My name is Franciscoelias, but everyone calls me Cisco. I entered the world of coding in 2021 with an online" \
-           "with a Udemy course bla bla. Since then,I have become familiar with programs such as bla bla. I have created" \
-           " some projects through my course, as well as individual projects that I have displayed on Github. I have also " \
-           "joined a group called bla bla, where I have done a presentation of a step-by-step on how to create a log-in " \
-           "bla bla for beginners.  Coding is a fascinating subject(?) for me, that I wish I found sooner. Being fluent " \
-           "in english, spanish and french (and conversational in others), I can't help but draw similarites between " \
-           "coding and learning a language, something I really enjoy. There is always something new to learn, and constant problem"\
-            "solving are other aspects of coding that really appeal to me (just to name a few.\n"\
-            "My previous work experience as a Scuba Diving Instructor, and a manager in hospitality have required of me " \
-           "a high level of dedication and self-motivation,professional work etiquette, ability to work under pressure " \
-           "and meet deadlines and a high level of communcation skill with people from all works of life."\
-           "In my spare time you can find me sailing, or scuba diving, reading about history, and of course with my family."\
-           "For more detail about my work experience, projects and skills, please follow the link to my Github and resume."\
-            "Kind regards, Cisco"
-
-testty= "<h1 class='helloh1'>vfvafvafvafvafvafva</h1>\n<br><h6>pararaeer</h6> "
-
 send = SendEmail()
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY']='fgwfnbg bjiopnegpbniegpbuio npbnwbwbwgbetybnetyne'
 @app.route('/', methods=['GET','POST'])
 def home():
-    about = AboutMe()
-    if about.validate_on_submit():
-        pass
-    return render_template('home.html', about=about)
+
+    return render_template('home.html')
 
 @app.route("/aboutme")
 def aboutme():
@@ -64,9 +41,6 @@ def contact():
 def projects():
     return render_template('projects.html')
 
-@app.route('/portafolio')
-def index():
-    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
